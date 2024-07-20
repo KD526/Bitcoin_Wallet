ff702911618be391a6c7f48a4f71abd3598b45f4
Kd526 <davisthoyah@gmail.com>
2024-07-20 22:36:14 +0300

    initial work

---
+++ wallet/target/debug/deps/wallet-dda21ad0ec43c9f0.d
@@ -0,0 +1,5 @@
+/home/kd526/rustacean/Bitcoin/Bitcoin_Wallet/wallet/target/debug/deps/wallet-dda21ad0ec43c9f0: src/main.rs
+
+/home/kd526/rustacean/Bitcoin/Bitcoin_Wallet/wallet/target/debug/deps/wallet-dda21ad0ec43c9f0.d: src/main.rs
+
+src/main.rs:
---
+++ wallet/target/debug/wallet.d
@@ -0,0 +1,1 @@
+/home/kd526/rustacean/Bitcoin/Bitcoin_Wallet/wallet/target/debug/wallet: /home/kd526/rustacean/Bitcoin/Bitcoin_Wallet/wallet/src/main.rs
