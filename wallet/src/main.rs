91c63d01739a4517d45fa62665459f9bc4b1d62d
Kd526 <davisthoyah@gmail.com>
2024-08-07 13:16:53 +0300

    code init

--- wallet/src/main.rs
+++ wallet/src/main.rs
@@ -1,3 +1,11 @@
+use dotenv::from_filename;
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Initiate wallet!....");
+    dotenv::from_filename(".env").ok();
+
+    let descriptor = env::var("WALLET_DESCRIPTOR").unwrap();
+    println!("Descriptor: {}", descriptor);
+
 }
